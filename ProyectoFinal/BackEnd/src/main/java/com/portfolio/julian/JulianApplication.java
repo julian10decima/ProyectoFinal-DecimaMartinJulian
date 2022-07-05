36ef4339f5022763b12e1783604fbad1cd6b0a4e
julian10decima <decimamartin10@gmail.com>
2022-07-05 12:16:21 -0300

    carpeta backend y conexion a base de datos con xamp

---
+++ ProyectoFinal/BackEnd/src/main/java/com/portfolio/julian/JulianApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.julian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulianApplication.class, args);
+	}
+
+}
---
+++ ProyectoFinal/BackEnd/src/test/java/com/portfolio/julian/JulianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.julian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
