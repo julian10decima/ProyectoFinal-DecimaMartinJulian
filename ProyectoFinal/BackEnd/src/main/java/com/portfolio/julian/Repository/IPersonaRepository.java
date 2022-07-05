189dec2b60d59aeb1fb815a0e1ab7cd4cf96998d
julian10decima <decimamartin10@gmail.com>
2022-07-05 14:48:33 -0300

    creando controller, service, repository, Interface

---
+++ ProyectoFinal/BackEnd/src/main/java/com/portfolio/julian/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.julian.Repository;
+
+import com.portfolio.julian.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona, Long>{
+    
+}
